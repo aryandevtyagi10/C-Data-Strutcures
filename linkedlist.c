6f14eb155cbc370e24d433a254f1133280c63298
aryandevtyagi10 <168193058+aryandevtyagi10@users.noreply.github.com>
2025-02-10 21:12:27 +0530

    Add files via upload

---
+++ linkedlist.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main(){
+
+    struct node{
+        int data;
+        struct node *next;
+    };
+    struct node *head, *newnode,*temp;
+    head = 0;
+    char choice;
+    while (choice){
+  newnode = (struct node*) malloc(sizeof(struct node));
+  printf("Enter data: ");
+  scanf("%d",&newnode-> data);
+  newnode->next = 0;
+
+  if (head == 0){
+    head = temp = newnode;
+  }
+  else {
+    temp ->next = newnode;
+    temp = newnode;
+  }
+  printf("Do you want to continue: (Y/N)");
+  scanf("%c",choice);
+    }
+    temp = head;
+    while(temp != 0){
+         printf("%d",temp->data);
+         temp = temp -> next ;
+    }
+}
