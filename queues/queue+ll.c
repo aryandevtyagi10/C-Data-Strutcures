b8af3fc042d25fc13dc32ed97064be12a389c15d
ADT <168193058+aryandevtyagi10@users.noreply.github.com>
2025-02-27 01:21:01 +0530

    Add files via upload

---
+++ queues/queue+ll.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node{
+    int data;
+    struct node*next;
+};
+struct node*front=NULL;
+struct node*rear=NULL;
+void enqueue(int x){
+    struct node*newnode=malloc(sizeof(struct node));
+    newnode->data=x;
+    newnode->next=NULL;
+    if(front==NULL && rear==NULL){
+        front=rear=newnode;
+    }
+    else{
+        rear->next=newnode;
+        rear=newnode;
+    }
+}
+void dequeue(){
+    struct node*temp=front;
+    if(front==NULL && rear==NULL){
+        printf("the queue is empty");
+    }
+    else{
+        front=front->next;
+        free(temp);
+    }
+}
+void display(){
+    struct node*ptr=front;
+    while(ptr!=NULL){
+        printf("%d \n",ptr->data);
+        ptr=ptr->next;
+    }
+}
+int main(){
+    enqueue(1);
+    enqueue(2);
+    enqueue(3);
+    display();
+    dequeue();
+    display();
+}
