156b7c5ae088c5c0d48ac758eec5bd9c92df87b1
ADT <168193058+aryandevtyagi10@users.noreply.github.com>
2025-03-07 23:48:25 +0530

    Add files via upload

---
+++ missingnumber.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int missingnum(int* nums, int size)
+sum = (size*(size+1))/2;
+int arsum=0;
+for(int i=0;i<size;i++){
+    arsum = arsum+ nums[i];
+}
+return (sum-arsum);
+}
